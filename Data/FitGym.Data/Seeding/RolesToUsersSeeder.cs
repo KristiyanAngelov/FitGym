32f1f6acfa885afa7fc5c5b4b1b3e5abad86b76c
Kristiyan Angelov <kristian.angelov@fdiba.tu-sofia.bg>
2020-11-13 18:48:24 +0200

    Add UsersSeeder and RolesToUsersSeeder

---
+++ Data/FitGym.Data/Seeding/RolesToUsersSeeder.cs
@@ -0,0 +1,33 @@
+﻿namespace FitGym.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using FitGym.Common;
+    using FitGym.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class RolesToUsersSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            await SeedRoleAsync(userManager, GlobalConstants.AdministratorRoleName, "admin@test.com");
+            await SeedRoleAsync(userManager, GlobalConstants.TrainerRoleName, "trainer@test.com");
+            await SeedRoleAsync(userManager, GlobalConstants.ClientRoleName, "client@test.com");
+        }
+
+        private static async Task SeedRoleAsync(UserManager<ApplicationUser> userManager, string roleName, string userEmail)
+        {
+            var user = await userManager.Users.FirstAsync(x => x.Email == userEmail);
+            if (user.Roles.Any() == false)
+            {
+                await userManager.AddToRoleAsync(user, roleName);
+            }
+        }
+    }
+}
---
+++ Data/FitGym.Data/Seeding/UsersSeeder.cs
@@ -0,0 +1,41 @@
+﻿namespace FitGym.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using FitGym.Data.Models;
+    using Microsoft.EntityFrameworkCore.Internal;
+
+    public class UsersSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Users.Any())
+            {
+                return;
+            }
+
+            var users = new List<(string Email, string Password)>
+            {
+                // 123456 -> hashed password "AQAAAAEAACcQAAAAEIBaDu0WstEaMQy6+oPrUFNAUYh8OcXd56pNJCbG/GcMfjnvwZO0WTWxnRonZUX3hQ=="
+                ("admin@test.com", "AQAAAAEAACcQAAAAEIBaDu0WstEaMQy6+oPrUFNAUYh8OcXd56pNJCbG/GcMfjnvwZO0WTWxnRonZUX3hQ=="),
+                ("trainer@test.com", "AQAAAAEAACcQAAAAEIBaDu0WstEaMQy6+oPrUFNAUYh8OcXd56pNJCbG/GcMfjnvwZO0WTWxnRonZUX3hQ=="),
+                ("client@test.com", "AQAAAAEAACcQAAAAEIBaDu0WstEaMQy6+oPrUFNAUYh8OcXd56pNJCbG/GcMfjnvwZO0WTWxnRonZUX3hQ=="),
+            };
+
+            foreach (var user in users)
+            {
+                await dbContext.Users.AddAsync(new ApplicationUser
+                {
+                    UserName = user.Email,
+                    Email = user.Email,
+                    PasswordHash = user.Password,
+                });
+            }
+        }
+    }
+}
