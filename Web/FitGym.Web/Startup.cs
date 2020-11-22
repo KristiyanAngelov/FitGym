afe1b297519afdbbe5ab5c4a618aab3fbf733c10
Kristiyan Angelov <kristian.angelov@fdiba.tu-sofia.bg>
2020-11-22 12:14:55 +0200

    Update to .NET 5.0, ASP.NET Core 5.0 and EF Core 5.0

--- Web/FitGym.Web/Startup.cs
+++ Web/FitGym.Web/Startup.cs
@@ -85,7 +85,7 @@
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
