ac3bd8770a6151a33bedfafca0836ad1a1d0b604
Kristiyan Angelov <kristian.angelov@fdiba.tu-sofia.bg>
2020-12-15 12:14:54 +0200

    Add CategoriesService unit tests

--- Services/FitGym.Services.Data/Interfaces/ICategoriesService.cs
+++ Services/FitGym.Services.Data/Interfaces/ICategoriesService.cs
@@ -7,8 +7,6 @@
     {
         IEnumerable<T> GetAll<T>(int? count = null);
 
-        IEnumerable<T> GetAllPosts<T>(int id);
-
         T GetByName<T>(string name);
 
         Task<int> CreateAsync(string name, string title, string description, string imageUrl);
---
+++ Tests/FitGym.Services.Data.Tests/CategoriesServiceTests.cs
@@ -0,0 +1,95 @@
+﻿namespace FitGym.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using FitGym.Data;
+    using FitGym.Data.Common.Repositories;
+    using FitGym.Data.Models;
+    using FitGym.Services.Mapping;
+    using FitGym.Web.ViewModels;
+    using FitGym.Web.ViewModels.Categories;
+    using FitGym.Web.ViewModels.Forum;
+    using Microsoft.EntityFrameworkCore;
+    using Moq;
+    using Xunit;
+
+    public class CategoriesServiceTests
+    {
+        public CategoriesServiceTests()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+        }
+
+        [Fact]
+        public async Task CreateMethodShouldAddNewCategorie()
+        {
+            var list = new List<Category>();
+            var mockRepo = new Mock<IDeletableEntityRepository<Category>>();
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Category>())).Callback((Category category) => list.Add(category));
+            var service = new CategoriesService(mockRepo.Object);
+            await service.CreateAsync("Cardio", "Cardio", "Description", "urlUrlurl");
+
+            Assert.Single(list);
+        }
+
+        [Fact]
+        public void GetAllShouldReturnAllCategories()
+        {
+            var mockRepo = new Mock<IDeletableEntityRepository<Category>>();
+            mockRepo.Setup(x => x.All())
+                .Returns(new List<Category>()
+                {
+                    new Category
+                    {
+                        Name = "Test",
+                        Title = "Test",
+                        Description = "Test",
+                        ImageUrl = "Test",
+                    },
+                    new Category
+                    {
+                        Name = "Test2",
+                        Title = "Test2",
+                        Description = "Test2",
+                        ImageUrl = "Test2",
+                    },
+                }.AsQueryable());
+            var service = new CategoriesService(mockRepo.Object);
+            var result = service.GetAll<IndexCategoryViewModel>();
+
+            Assert.Equal(2, result.Count());
+        }
+
+        [Fact]
+        public void GetByNameShouldReturnCorrectCategory()
+        {
+            var mockRepo = new Mock<IDeletableEntityRepository<Category>>();
+            mockRepo.Setup(x => x.All())
+                .Returns(new List<Category>()
+                {
+                    new Category
+                    {
+                        Name = "Test",
+                        Title = "Test",
+                        Description = "Test",
+                        ImageUrl = "Test",
+                    },
+                    new Category
+                    {
+                        Name = "Test2",
+                        Title = "Test2",
+                        Description = "Test2",
+                        ImageUrl = "Test2",
+                    },
+                }.AsQueryable());
+            var service = new CategoriesService(mockRepo.Object);
+            var result = service.GetByName<CategoryViewModel>("Test2");
+
+            Assert.Equal("Test2", result.Title);
+        }
+    }
+}
