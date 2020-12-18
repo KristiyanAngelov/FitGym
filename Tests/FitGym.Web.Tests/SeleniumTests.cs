66c60bfe5f273ad850405908318b5e092b98213c
Kristiyan Angelov <kristian.angelov@fdiba.tu-sofia.bg>
2020-12-18 13:24:27 +0200

    Add Selenium test

--- Tests/FitGym.Web.Tests/SeleniumTests.cs
+++ Tests/FitGym.Web.Tests/SeleniumTests.cs
@@ -1,35 +1,59 @@
 ﻿namespace FitGym.Web.Tests
 {
+    using System;
+    using System.Linq;
+
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
     using OpenQA.Selenium.Remote;
-
     using Xunit;
 
-#pragma warning disable CA1001 // Types that own disposable fields should be disposable
-    public class SeleniumTests : IClassFixture<SeleniumServerFactory<Startup>>
-#pragma warning restore CA1001 // Types that own disposable fields should be disposable
+    public class SeleniumTests : IClassFixture<SeleniumServerFactory<Startup>>, IDisposable
     {
         private readonly SeleniumServerFactory<Startup> server;
         private readonly IWebDriver browser;
 
-        // Be sure that selenium-server-standalone-3.141.59.jar is running
         public SeleniumTests(SeleniumServerFactory<Startup> server)
         {
             this.server = server;
             server.CreateClient();
             var opts = new ChromeOptions();
-            opts.AddArguments("--headless", "--ignore-certificate-errors");
-            this.browser = new RemoteWebDriver(opts);
+            opts.AddArguments("--headless");
+            opts.AcceptInsecureCertificates = true;
+            this.browser = new ChromeDriver(opts);
         }
 
-        [Fact(Skip = "Example test. Disabled for CI.")]
+        [Fact]
         public void FooterOfThePageContainsPrivacyLink()
         {
             this.browser.Navigate().GoToUrl(this.server.RootUri);
-            Assert.Contains(
-                this.browser.FindElements(By.CssSelector("footer a")),
-                x => x.GetAttribute("href").EndsWith("/Home/Privacy"));
+            Assert.EndsWith(
+                "/Home/Privacy",
+                this.browser.FindElements(By.CssSelector("footer a")).First().GetAttribute("href"));
+        }
+
+        [Fact]
+        public void TrainerPicturesUrlShouldEndWithJPG()
+        {
+            this.browser.Navigate().GoToUrl(this.server.RootUri + "/Home/About?area=%2F");
+            Assert.Equal(
+                "Gym",
+                this.browser.FindElements(By.TagName("h2")).First().Text);
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.server?.Dispose();
+                this.browser?.Dispose();
+            }
         }
     }
 }
